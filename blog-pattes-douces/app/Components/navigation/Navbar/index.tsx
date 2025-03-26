a1204e06f30b999cc2a859d2f856316f0d8b2d39
Alexandre-SDV <gouraudalex35@gmail.com>
2025-03-26 14:15:03 +0100

    Correction Navbar + Création Profile
    Correction Navbar

--- blog-pattes-douces/app/Components/navigation/Navbar/index.tsx
+++ blog-pattes-douces/app/Components/navigation/Navbar/index.tsx
@@ -1,25 +1,30 @@
 import React from "react";
 import Link from "next/link";
 import Feedhome from "../../feed/Feedhome";
+import Profile from "../../pages/profile";
 // import Logo from "./Components/Navbar/Logo";
 // import Button from "./Components/Navbar/Navbar";
 
 const Navbar = () => {
   return (
-    <>
     <div className="navbar">
-      <div className="w-full h-20" style={{ background: "linear-gradient(90deg, hsla(28, 38%, 43%, 1) 0%, hsla(0, 0%, 27%, 1) 100%)" }}> {/*  //Couleur du Background */}
+      <div
+        className="w-full h-20"
+        style={{
+          background:
+            "linear-gradient(90deg, hsla(28, 38%, 43%, 1) 0%, hsla(0, 0%, 27%, 1) 100%)",
+        }}
+      >
         <div className="container mx-auto px-4 h-full">
-          <div className="flex justify-end items-center h-full"> {/* Texte aligné à droite */}
-            {/* <Logo /> */}
+          <div className="flex justify-end items-center h-full">
             <ul className="hidden md:flex gap-x-6 text-white">
               <li>
-                <Link href="/profil">
+                <Link href="/profile">
                   <p>Profile</p>
                 </Link>
               </li>
               <li>
-                <Link href="</feedhome>">
+                <Link href="/feed">
                   <p>Feed</p>
                 </Link>
               </li>
@@ -34,12 +39,10 @@ const Navbar = () => {
                 </Link>
               </li>
             </ul>
-            {/* <Button /> */}
           </div>
         </div>
       </div>
     </div>
-    </>
   );
 };
 
