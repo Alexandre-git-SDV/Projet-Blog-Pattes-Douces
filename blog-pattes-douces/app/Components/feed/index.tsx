c2b5814be32ade9f54b587a6fe43f27630ac866e
Alexandre-SDV <gouraudalex35@gmail.com>
2025-04-01 15:11:23 +0200

    Correction Feed + Commentaire
    Correction Feed + Commentaire
    
    Ajout des fetch auteur des articles
    
    Et correction Commentaire

--- blog-pattes-douces/app/Components/feed/index.tsx
+++ blog-pattes-douces/app/Components/feed/index.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 
 type Article = {
   id: string;
+  auteur: User
   titre: string;
   texte: string;
   image?: string;
@@ -11,6 +12,12 @@ type Article = {
   reaction1: any[];
   reaction2: any[];
 };
+
+type User = {
+  id: string;
+  pseudo: string;
+  email: string;
+}
 
 export default function Feedhome() {
   const [pseudo, setPseudo] = useState<string | null>(null);
@@ -83,9 +90,9 @@ export default function Feedhome() {
         {articles.map((article) => (
           <div
             key={article.id}
-            className="block transform transition-transform duration-300 hover:scale-105 bg-white p-4 rounded-md shadow-md"
+            className="block transform transition-transform duration-300 hover:scale-102 bg-white p-4 rounded-md shadow-md"
           >
-            <h2 className="text-xl font-semibold">{pseudo}</h2>
+            <h2 className="text-xl font-semibold">{article.auteur?.pseudo || "Inconnu"}</h2>
             <h2 className="text-xl font-semibold">{article.titre}</h2>
             <p className="text-gray-700">{article.texte}</p>
             {article.image && (
@@ -100,7 +107,7 @@ export default function Feedhome() {
             </p>
 
             <div className="flex space-x-4">
-              <p className="text-sm text-green-400">Vues : {article.vue.length}</p>
+              <p className="text-sm text-gray-400">Vues : {article.vue.length}</p>
               <p className="text-sm text-blue-400">Like : {article.reaction1.length}</p>
               <p className="text-sm text-red-400">Dislike : {article.reaction2.length}</p>
             </div>
