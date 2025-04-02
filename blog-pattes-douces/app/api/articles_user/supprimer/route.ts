4f64e318f6c95ebc84fc11431f8b45b8b2d7d6b0
Aldin285 <148047232+Aldin285@users.noreply.github.com>
2025-04-02 14:08:40 +0200

    réglage de la fonction suppression article

--- blog-pattes-douces/app/api/articles_user/supprimer/route.ts
+++ blog-pattes-douces/app/api/articles_user/supprimer/route.ts
@@ -4,6 +4,10 @@ import {prisma} from "@/src/db/prisma";
 export async function POST(req: Request) {
     try {
       const { id } = await req.json();
+      await prisma.commentaire.deleteMany({
+        where: { article_sourceId: id }
+    });
+    
       await prisma.article.delete({ where: { id } });
   
       return NextResponse.json({ success: true });
